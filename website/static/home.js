13b1c7e5702db3d2c1e2d7404f1679dfea98461f
amine17177272 <amnmed00@gmail.com>
2024-06-11 20:57:51 +0100

    upload

---
+++ website/static/home.js
@@ -0,0 +1,13 @@
+console.log("JavaScript file is loaded and working.");
+const menu = document.querySelector('.menu');
+const links = document.querySelectorAll('.menu a');
+
+function activateParallax() {
+  links.forEach((link, i) => {
+    link.addEventListener('mouseover', () => {
+      menu.dataset.index = i
+    }, false);
+  });
+}
+
+window.addEventListener('load', activateParallax, false);
